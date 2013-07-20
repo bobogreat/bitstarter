dceab1ce49d13fc7355d768b784e09c04c5cad8c
TomyOne <galbicka.tomas@gmail.com>
2013-07-20 02:56:20 +0000

    sdasda

--- web.js
+++ web.js
@@ -2,8 +2,10 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var index = fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Hello World! 33' + index );
 });
 
 var port = process.env.PORT || 5000;
